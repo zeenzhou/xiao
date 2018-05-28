4c767eb6acae4d98d3e57facad942d2aff63fed1
zhouliang <zhouliang@172.16.0.95>
2018-05-28 10:26:40 +0800

    config env

---
+++ src/com/zeen/test/Hello.java
@@ -0,0 +1,27 @@
+/*************************************************************************
+ *                  HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ *
+ *                COPYRIGHT (C) HONGLING CAPITAL CORPORATION 2012
+ *    ALL RIGHTS RESERVED BY HONGLING CAPITAL CORPORATION. THIS PROGRAM
+ * MUST BE USED  SOLELY FOR THE PURPOSE FOR WHICH IT WAS FURNISHED BY
+ * HONGLING CAPITAL CORPORATION. NO PART OF THIS PROGRAM MAY BE REPRODUCED
+ * OR DISCLOSED TO OTHERS,IN ANY FORM, WITHOUT THE PRIOR WRITTEN
+ * PERMISSION OF HONGLING CAPITAL CORPORATION. USE OF COPYRIGHT NOTICE
+ DOES NOT EVIDENCE PUBLICATION OF THE PROGRAM.
+ *                  HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ *************************************************************************/
+package com.zeen.test;
+
+import javax.sound.midi.Soundbank;
+
+/**
+ * Hello world
+ *
+ * @author zhouliang at 2018/5/28 10:22
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
